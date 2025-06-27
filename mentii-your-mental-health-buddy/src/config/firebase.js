7a57ce68e834633d2d3726b570a8e8981f6b87a0
SUCHITA2301 <suchita.st.23@gmail.com>
2025-06-27 21:52:43 +0530

    Fixed Firebase config with VITE env variables

--- mentii-your-mental-health-buddy/src/config/firebase.js
+++ mentii-your-mental-health-buddy/src/config/firebase.js
@@ -5,13 +5,13 @@ import { getFirestore, collection, getDocs, addDoc, query, orderBy  } from 'fire
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey: import.meta.env.FIREBASE_API,
-  authDomain: import.meta.env.AuthDomain,
-  projectId: import.meta.env.projectId,
-  storageBucket: import.meta.env.storageBucket,
-  messagingSenderId: import.meta.env.messagingSenderId,
-  appId: import.meta.env.appId,
-  measurementId: import.meta.env.measurementId
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase only if it hasn't been initialized
